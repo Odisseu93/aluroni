fc9e0c01bbb6969be4f4dd4411ea8a5284d634ef
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-09-30 20:38:50 -0300

    alterando a url para encontar a logo

--- src/pages/cardapio/index.tsx
+++ src/pages/cardapio/index.tsx
@@ -1,5 +1,5 @@
 import styles from './Cardapio.module.scss';
-import { ReactComponent as Logo} from '../../assets/logo.svg' /* Gera o svg no build*/
+import { ReactComponent as Logo} from 'assets/logo.svg' /* Gera o svg no build*/
 
 export default function Cardapio() {
     return (
