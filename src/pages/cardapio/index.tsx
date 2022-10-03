11feb0529d272cca88a23fc1cc0a65465fb4dbd2
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-10-02 21:38:53 -0300

    criando o header - adicionando uma imagem de background - criando os estilos - adicionando o header no index.tsx

--- src/pages/cardapio/index.tsx
+++ src/pages/cardapio/index.tsx
@@ -1,12 +1,17 @@
 import styles from './Cardapio.module.scss';
-import { ReactComponent as Logo} from 'assets/logo.svg' /* Gera o svg no build*/
+import { ReactComponent as Logo } from 'assets/logo.svg' /* Gera o svg no build*/
 
 export default function Cardapio() {
     return (
         <main>
             <nav className={styles.menu}>
-                <Logo />    
+                <Logo />
             </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A casa do código e da massa
+                </div>
+            </header>
         </main>
     );
 }
