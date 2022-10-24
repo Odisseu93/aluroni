81621000f43665b803cdcde554f715cbb29036d1
Ulisses <ulissessuporteti01@gmail.com>
2022-10-24 13:23:09 -0300

    importando o Router Dom

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
-import Cardapio from './pages/cardapio';
-import Inicio from 'pages/inicio';
+import Router  from './routes';
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Inicio />
+    <Router />
   </React.StrictMode>
 );
 
