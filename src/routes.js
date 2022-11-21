d75bfc14f8a6d1d75b2ec737547a4a481281eaae
Ulisses <ulissessuporteti01@gmail.com>
2022-11-21 19:06:51 -0300

    criando uma rota para sobre

--- src/routes.js
+++ src/routes.js
@@ -2,20 +2,22 @@ import Menu from 'assets/components/Menu';
 import PaginaPadrao from 'assets/components/PaginaPadrao';
 import Cardapio from 'pages/cardapio';
 import Inicio from 'pages/inicio';
+import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
   return (
     <main>
-      <Router> 
+      <Router>
         <Menu />
         <Routes>
-          <Route path='/' element={<PaginaPadrao />}>
+          <Route path="/" element={<PaginaPadrao />}>
             <Route index element={<Inicio />} />
-            <Route path='cardapio' element={<Cardapio />} />
+            <Route path="cardapio" element={<Cardapio />} />
+            <Route path="sobre" element={<Sobre />} />
           </Route>
         </Routes>
-      </ Router >
+      </Router>
     </main>
   );
 }
